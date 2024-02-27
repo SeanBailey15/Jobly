55f63d52d2724db1d355d45503d2899638c9f48b
Sean Bailey <seanbailey15@gmail.com>
2024-02-26 20:58:24 -0500

    added filter functionality to GET /companies allowing users to filter by partial name, min employees, and max employees. routes/companies contains logic to validate request query string data. continued this functionality in models/company Company.findAll method. logic to generate dynamic database query and submit the query, as well as error handling are written within that method

--- models/company.js
+++ models/company.js
@@ -1,7 +1,11 @@
 "use strict";
 
 const db = require("../db");
-const { BadRequestError, NotFoundError } = require("../expressError");
+const {
+  BadRequestError,
+  NotFoundError,
+  ExpressError,
+} = require("../expressError");
 const { sqlForPartialUpdate } = require("../helpers/sql");
 
 /** Related functions for companies. */
@@ -18,26 +22,21 @@ class Company {
 
   static async create({ handle, name, description, numEmployees, logoUrl }) {
     const duplicateCheck = await db.query(
-          `SELECT handle
+      `SELECT handle
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]
+    );
 
     if (duplicateCheck.rows[0])
       throw new BadRequestError(`Duplicate company: ${handle}`);
 
     const result = await db.query(
-          `INSERT INTO companies
+      `INSERT INTO companies
            (handle, name, description, num_employees, logo_url)
            VALUES ($1, $2, $3, $4, $5)
            RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`,
-        [
-          handle,
-          name,
-          description,
-          numEmployees,
-          logoUrl,
-        ],
+      [handle, name, description, numEmployees, logoUrl]
     );
     const company = result.rows[0];
 
@@ -49,16 +48,76 @@ class Company {
    * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
    * */
 
-  static async findAll() {
-    const companiesRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           ORDER BY name`);
-    return companiesRes.rows;
+  static async findAll(filterBy = undefined) {
+    if (filterBy === undefined) {
+      const companiesRes = await db.query(
+        `SELECT handle,
+                    name,
+                    description,
+                    num_employees AS "numEmployees",
+                    logo_url AS "logoUrl"
+             FROM companies
+             ORDER BY name`
+      );
+
+      if (companiesRes.rows.length === 0)
+        throw new ExpressError("No companies exist in database", 404);
+
+      return companiesRes.rows;
+    }
+    let filterQueries = [];
+    let values = [];
+    let idx = 1;
+    for (let key in filterBy) {
+      if (idx === 1) {
+        if (key === "nameLike") {
+          filterQueries.push(`WHERE name ILIKE $${idx}`);
+          values.push(`%${filterBy[key]}%`);
+          idx += 1;
+        } else if (key === "minEmployees") {
+          filterQueries.push(`WHERE num_employees >= $${idx}`);
+          values.push(+filterBy[key]);
+          idx += 1;
+        } else if (key === "maxEmployees") {
+          filterQueries.push(`WHERE num_employees <= $${idx}`);
+          values.push(+filterBy[key]);
+          idx += 1;
+        }
+      } else {
+        if (key === "nameLike") {
+          filterQueries.push(`name ILIKE $${idx}`);
+          values.push(`%${filterBy[key]}%`);
+          idx += 1;
+        } else if (key === "minEmployees") {
+          filterQueries.push(`num_employees >= $${idx}`);
+          values.push(+filterBy[key]);
+          idx += 1;
+        } else if (key === "maxEmployees") {
+          filterQueries.push(`num_employees <= $${idx}`);
+          values.push(+filterBy[key]);
+          idx += 1;
+        }
+      }
+    }
+    const joinedQueries = filterQueries.join(" AND ");
+    console.log(joinedQueries);
+    console.log(values);
+    const filteredRes = await db.query(
+      `SELECT handle,
+              name,
+              description,
+              num_employees AS "numEmployees",
+              logo_url AS "logoUrl"
+        FROM companies
+        ${joinedQueries}
+        ORDER BY name`,
+      values
+    );
+
+    if (filteredRes.rows.length === 0)
+      throw new ExpressError("No companies match the parameters", 404);
+
+    return filteredRes.rows;
   }
 
   /** Given a company handle, return data about company.
@@ -71,14 +130,15 @@ class Company {
 
   static async get(handle) {
     const companyRes = await db.query(
-          `SELECT handle,
+      `SELECT handle,
                   name,
                   description,
                   num_employees AS "numEmployees",
                   logo_url AS "logoUrl"
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]
+    );
 
     const company = companyRes.rows[0];
 
@@ -100,12 +160,10 @@ class Company {
    */
 
   static async update(handle, data) {
-    const { setCols, values } = sqlForPartialUpdate(
-        data,
-        {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url",
-        });
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      numEmployees: "num_employees",
+      logoUrl: "logo_url",
+    });
     const handleVarIdx = "$" + (values.length + 1);
 
     const querySql = `UPDATE companies 
@@ -131,16 +189,16 @@ class Company {
 
   static async remove(handle) {
     const result = await db.query(
-          `DELETE
+      `DELETE
            FROM companies
            WHERE handle = $1
            RETURNING handle`,
-        [handle]);
+      [handle]
+    );
     const company = result.rows[0];
 
     if (!company) throw new NotFoundError(`No company: ${handle}`);
   }
 }
-
 
 module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -5,7 +5,7 @@
 const jsonschema = require("jsonschema");
 const express = require("express");
 
-const { BadRequestError } = require("../expressError");
+const { BadRequestError, ExpressError } = require("../expressError");
 const { ensureLoggedIn } = require("../middleware/auth");
 const Company = require("../models/company");
 
@@ -13,7 +13,6 @@ const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
 
 const router = new express.Router();
-
 
 /** POST / { company } =>  { company }
  *
@@ -28,7 +27,7 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyNewSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -52,8 +51,53 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
 
 router.get("/", async function (req, res, next) {
   try {
-    const companies = await Company.findAll();
-    return res.json({ companies });
+    // Declare variable to contain request query object
+    const filterBy = req.query;
+
+    // Declare variables to contain min/max employees if applicable
+    let minEmployees;
+    let maxEmployees;
+
+    // Loop through provided query params and verify they are relevant to the data available
+    for (let key in filterBy) {
+      // Checks that provided params match what is expected, throws error if not
+      if (key != "nameLike" && key != "minEmployees" && key != "maxEmployees") {
+        throw new ExpressError(`Cannot filter results by ${key}`, 400);
+      }
+      // Checks for missing values and throws error if so
+      else if (filterBy[key] === "") {
+        throw new ExpressError(`Missing value for parameter ${key}`, 400);
+      }
+      // Sets minEmployees variable if exists, converts the string to a number
+      else if (key === "minEmployees") {
+        minEmployees = +filterBy[key];
+      }
+      // Sets maxEmployees variable if exists, converts the string to a number
+      else if (key === "maxEmployees") {
+        maxEmployees = +filterBy[key];
+      }
+
+      // If min value provided is greater than max value provided, throws an error
+      if (minEmployees > maxEmployees) {
+        throw new ExpressError(
+          "Minimum employees cannot be greater than maximum",
+          400
+        );
+      }
+    }
+
+    // If no query params are passed, get all companies unfiltered
+    if (Object.keys(filterBy).length === 0) {
+      const companies = await Company.findAll();
+      return res.json({ companies });
+    }
+
+    // Get all companies by passing the filterBy object to the Company.findAll method
+    const filteredCompanies = await Company.findAll(filterBy);
+
+    return res.json({
+      companies: { filteredCompanies },
+    });
   } catch (err) {
     return next(err);
   }
@@ -91,7 +135,7 @@ router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyUpdateSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -115,6 +159,5 @@ router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
     return next(err);
   }
 });
-
 
 module.exports = router;
